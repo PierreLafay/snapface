2b0a9a98120643fa25ce2531a2384754c9f85cb6
PierreLafay <p.lafay@ellipse-online.com>
2022-07-25 05:39:45 +0200

    Après partie 2

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaceSnapComponent implements OnInit {
 
-  constructor() { }
+  @Input() faceSnap!: FaceSnap;
+  buttonText!: string;
 
   ngOnInit(): void {
+     this.buttonText = 'Ajouter like';
   }
-
+  onLike() {
+    if (this.buttonText === 'Ajouter like') {
+      this.faceSnap.likes++;
+      this.buttonText = 'Supprimer like';
+    } else {
+      this.faceSnap.likes--;
+      this.buttonText = 'Ajouter like';
+    }
+  }
 }
