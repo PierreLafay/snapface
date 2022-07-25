fd4a0220d944a08392ff40f40fbcf6bd9d3991b4
PierreLafay <p.lafay@ellipse-online.com>
2022-07-25 10:31:49 +0200

    Partie 4, 1ère étape sur le service, on y stocke les données dans une prop tableau de snaps

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,37 +11,10 @@ export class FaceSnapListComponent implements OnInit {
 
   faceSnaps!: FaceSnap[];
 
-  ngOnInit() {
+  constructor(private faceSnapsService: FaceSnapsService) { }
 
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        likes: 25,
-        price: 10.5,
-        location: 'Paris'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        likes: 15,
-        price: 3250,
-        location: 'La montagne'
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        likes: 0,
-        price: 9.99
-      }
-    ];
-
+  ngOnInit() : void {
+    this.faceSnaps = this.faceSnapsService.faceSnaps;
   }
 
 
--- src/app/services/face-snap.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root' // enregistré à la racine de l'app, partagé entre les composants
-})
-export class FaceSnapsService {
-
-}
