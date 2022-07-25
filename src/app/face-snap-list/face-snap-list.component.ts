23ec10039509ff76992fa8041d897e286e54841e
PierreLafay <p.lafay@ellipse-online.com>
2022-07-25 10:00:50 +0200

    Partie 4, après mise en place du composant face-snap-list et génération du service face-snap.service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaceSnapListComponent implements OnInit {
 
-  constructor() { }
+  faceSnaps!: FaceSnap[];
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        likes: 25,
+        price: 10.5,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        likes: 15,
+        price: 3250,
+        location: 'La montagne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        likes: 0,
+        price: 9.99
+      }
+    ];
+
   }
+
 
 }
---
+++ src/app/services/face-snap.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' // enregistré à la racine de l'app, partagé entre les composants
+})
+export class FaceSnapsService {
+
+}
