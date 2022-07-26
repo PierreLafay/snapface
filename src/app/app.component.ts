3e5b4276a74150acab276178d0131cddf6f6868d
PierreLafay <p.lafay@ellipse-online.com>
2022-07-26 05:59:31 +0200

    compléments chap 2 - 3 effet secondaire sur observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {filter, interval, map, Observable} from "rxjs";
+import {filter, interval, map, Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,13 +10,18 @@ export class AppComponent {
 
   interval$!: Observable<string>;
 
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+  }
+
   ngOnInit() {
     this.interval$ = interval(1000).pipe(
       filter( value => value % 3 === 0),
       map(value => value % 2 === 0 ?
         `Je suis ${value} et je suis pair` :
         `Je suis ${value} et je suis impair`
-      )
+      ),
+      tap(text => this.logger(text))
     );
   }
 
