652be0204b1b8156f09d2799bac9d97baa5e4a6f
PierreLafay <p.lafay@ellipse-online.com>
2022-07-26 07:00:25 +0200

    compléments chap 5 - 2 stratégie unsubscribe 2 : unsubscribe dans la fin du composant (page par ex) (Dans faceSnapList)

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
-import {interval, take, tap} from "rxjs";
+import {interval, Subject, takeUntil, tap} from "rxjs";
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+
+  // Obersavle permettant de gérer la fin de l'observable interval
+  private destroy$!: Subject<boolean>
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit() : void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
 
+    this.destroy$ = new Subject<boolean>();
+
     interval(1000).pipe(
-      take(3),
-      tap( value => console.log(value))
-// Ecriture simplifiée
-//      tap(console.log)
+      tap(console.log),
+      takeUntil(this.destroy$)
     ).subscribe();
   }
 
-
+  ngOnDestroy() {
+    this.destroy$.next(true);
+  }
 }
