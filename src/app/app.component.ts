0be12b37f4fc8672104dabcf688996952bb1f551
PierreLafay <p.lafay@ellipse-online.com>
2022-07-26 06:31:49 +0200

    compléments chap 4 - 1 combinaison d'observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
       map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
       tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
       // Modifier la méthode pour voir les différences
-      mergeMap(color => this.getTrainObservable$(color)),
+      switchMap(color => this.getTrainObservable$(color)),
       tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
     ).subscribe();
   }
