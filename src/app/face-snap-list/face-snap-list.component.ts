ab80f79ec0d9d8517934ef4e94ea4cc3a33cb453
PierreLafay <p.lafay@ellipse-online.com>
2022-07-26 06:49:03 +0200

    compléments chap 5 - 1 stratégie unsubscribe 1 : Take(n) (Dans faceSnapList)

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import {interval, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-face-snap-list',
@@ -15,6 +16,13 @@ export class FaceSnapListComponent implements OnInit {
 
   ngOnInit() : void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(
+      take(3),
+      tap( value => console.log(value))
+// Ecriture simplifiée
+//      tap(console.log)
+    ).subscribe();
   }
 
 
