1f27a0d45108b8f6ec310ae29129817e74ac81a6
PierreLafay <p.lafay@ellipse-online.com>
2022-07-27 14:37:15 +0200

    Compléments chap 16 : Lazzy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
-import {FaceSnapsModule} from "./face-snaps/face-snaps.module";
 import {LandingPageModule} from "./landing-page/landing-page.module";
 
 @NgModule({
@@ -17,7 +16,6 @@ import {LandingPageModule} from "./landing-page/landing-page.module";
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    FaceSnapsModule,
     LandingPageModule
   ],
   providers: [],
